dcc92cd17efc2fd200c402a9a9116d262bd0cc9a
Kristian Binau <kristian@binau.me>
2023-09-17 19:10:39 +0200

    #14 - Install Prettier and add configs

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+/** @type {import("@types/prettier").Options} */
+// Extracted from `webdev-configs` (https://github.com/JulianCataldo/web-garden/tree/develop/configs)
+
+module.exports = {
+	printWidth: 100,
+	semi: true,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'all',
+
+	// See: https://github.com/prettier/prettier/issues/7475
+	useTabs: true,
+};
